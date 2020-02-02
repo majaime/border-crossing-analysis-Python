b6a09eca2ac7234aabf6716ff8f58c76a52d385f
majaime <56524581+majaime@users.noreply.github.com>
2020-02-02 14:11:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# First we need to provide the root access
-#sudo chmod 777 ~/Dropbox/border-crossing-analysis-Python/run.sh
-#sudo chmod 777 ~/Dropbox/border-crossing-analysis-Python/insight_testsuite/run_tests.sh
 
-# Compiling the code
-#java src/Main.java
-#Python src/border-crossing-analysis.py
-
-# Returning to the src root and running the code
-#cd src
-#java Main
-#Python border-crossing-analysis.py
-#sudo chmod 777 ~/Dropbox/border-crossing-analysis-Python/src/border_crossing_analysis.py
 python ./src/border_crossing_analysis.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
